5010a1191a1d125258fb1f8d440e2974829bdecd
xoron <rmaher84@gmail.com>
2023-07-16 17:38:53 +0000

    Deploying to gh-pages from @ sytac-webpack-module-federation/website@d15462fd6b5d6ac5de5ecc3d5872651097e814a0 🚀

--- main.js
+++ main.js
@@ -115,6 +115,36 @@ module.exports = new Promise((resolve, reject) => {
 /******/ 		};
 /******/ 	})();
 /******/ 	
+/******/ 	/* webpack/runtime/create fake namespace object */
+/******/ 	(() => {
+/******/ 		var getProto = Object.getPrototypeOf ? (obj) => (Object.getPrototypeOf(obj)) : (obj) => (obj.__proto__);
+/******/ 		var leafPrototypes;
+/******/ 		// create a fake namespace object
+/******/ 		// mode & 1: value is a module id, require it
+/******/ 		// mode & 2: merge all properties of value into the ns
+/******/ 		// mode & 4: return value when already ns object
+/******/ 		// mode & 16: return value when it's Promise-like
+/******/ 		// mode & 8|1: behave like require
+/******/ 		__webpack_require__.t = function(value, mode) {
+/******/ 			if(mode & 1) value = this(value);
+/******/ 			if(mode & 8) return value;
+/******/ 			if(typeof value === 'object' && value) {
+/******/ 				if((mode & 4) && value.__esModule) return value;
+/******/ 				if((mode & 16) && typeof value.then === 'function') return value;
+/******/ 			}
+/******/ 			var ns = Object.create(null);
+/******/ 			__webpack_require__.r(ns);
+/******/ 			var def = {};
+/******/ 			leafPrototypes = leafPrototypes || [null, getProto({}), getProto([]), getProto(getProto)];
+/******/ 			for(var current = mode & 2 && value; typeof current == 'object' && !~leafPrototypes.indexOf(current); current = getProto(current)) {
+/******/ 				Object.getOwnPropertyNames(current).forEach((key) => (def[key] = () => (value[key])));
+/******/ 			}
+/******/ 			def['default'] = () => (value);
+/******/ 			__webpack_require__.d(ns, def);
+/******/ 			return ns;
+/******/ 		};
+/******/ 	})();
+/******/ 	
 /******/ 	/* webpack/runtime/define property getters */
 /******/ 	(() => {
 /******/ 		// define getter functions for harmony exports
@@ -235,20 +265,22 @@ module.exports = new Promise((resolve, reject) => {
 /******/ 	(() => {
 /******/ 		var chunkMapping = {
 /******/ 			"bootstrap_js": [
-/******/ 				"webpack/container/remote/signup/Signup",
 /******/ 				"webpack/container/remote/homepage/Homepage"
+/******/ 			],
+/******/ 			"webpack_container_remote_signup_Signup": [
+/******/ 				"webpack/container/remote/signup/Signup"
 /******/ 			]
 /******/ 		};
 /******/ 		var idToExternalAndNameMapping = {
-/******/ 			"webpack/container/remote/signup/Signup": [
-/******/ 				"default",
-/******/ 				"./Signup",
-/******/ 				"webpack/container/reference/signup"
-/******/ 			],
 /******/ 			"webpack/container/remote/homepage/Homepage": [
 /******/ 				"default",
 /******/ 				"./Homepage",
 /******/ 				"webpack/container/reference/homepage"
+/******/ 			],
+/******/ 			"webpack/container/remote/signup/Signup": [
+/******/ 				"default",
+/******/ 				"./Signup",
+/******/ 				"webpack/container/reference/signup"
 /******/ 			]
 /******/ 		};
 /******/ 		__webpack_require__.f.remotes = (chunkId, promises) => {
@@ -570,7 +602,7 @@ module.exports = new Promise((resolve, reject) => {
 /******/ 					if(installedChunkData) {
 /******/ 						promises.push(installedChunkData[2]);
 /******/ 					} else {
-/******/ 						if(!/^webpack_sharing_consume_default_react_react\-_(6492|c217)$/.test(chunkId)) {
+/******/ 						if(/^(vendors\-node_modules_react(|\-dom|\-router\-dom_dist)_index_js|bootstrap_js|main)$/.test(chunkId)) {
 /******/ 							// setup Promise in chunk cache
 /******/ 							var promise = new Promise((resolve, reject) => (installedChunkData = installedChunks[chunkId] = [resolve, reject]));
 /******/ 							promises.push(installedChunkData[2] = promise);
